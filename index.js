02c78d9f9cde4882e0379161a13cadd35f5ca001
RFowlerDB <rachie.fowler@db.com>
2022-09-28 15:44:59 -0400

    first commit for Morse code project

---
+++ index.js
@@ -0,0 +1,55 @@
+//make a function convertToMorse 
+//parameters {string}
+//return: string (morse code)
+//create an object with the morse code translation
+
+const morsecode =
+{
+   "A": ".-",
+   "B": "-...",
+   "C": "-.-.",
+   "D": "-..",
+   "E": ".",
+   "F": "..-.",
+   "G": "--.",
+   "H": "....",
+   "I": "..",
+   "J": ".---",
+   "K": "-.-",
+   "L": ".-..",
+   "M": "--",
+   "N": "-.",
+   "O": "---",
+   "P": ".--.",
+   "Q": "--.-",
+   "R": ".-.",
+   "S": "...",
+   "T": "-",
+   "U": "..-",
+   "W": ".--",
+   "X": "-..-",
+   "Y": "-.--",
+   "Z": "--.."
+}
+
+
+// export
+//  const convertToMorse = (word) => {
+//     return morsecode [word]
+// };
+// RETURNED: [Running] node "c:\Users\Chess\nology\Javascript\MorseCode_Project\index.js"
+// -...
+
+
+// export 
+const convertToMorse = (word) => {
+    let finalMorseCode = '';
+
+    for(let i=0; i<word.length; i++){
+        finalMorseCode += morsecode[word[i]];
+    };
+    return finalMorseCode;
+}
+
+ 
+ console.log(convertToMorse("BDC"));
